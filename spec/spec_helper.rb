9a319c10c3cfa40e5e1466309d10624074bef435
Alexander Popov <alex.wayfer@gmail.com>
2021-02-24 22:51:27 +0300

    Add Codecov and SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,4 +2,12 @@
 
 require 'pp'
 
+require 'simplecov'
+SimpleCov.start
+
+if ENV['CODECOV_TOKEN']
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require_relative '../lib/r18n-desktop'
