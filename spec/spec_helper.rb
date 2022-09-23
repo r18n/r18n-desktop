3cbc5e5b48102836bfdde3bcec972d85e62cecdb
Alexander Popov <alex.wayfer@gmail.com>
2022-09-23 04:42:39 +0300

    Replace `codecov` Ruby uploader with shell one

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,11 +3,12 @@
 require 'pp'
 
 require 'simplecov'
-SimpleCov.start
 
-if ENV['CODECOV_TOKEN']
-  require 'codecov'
-  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+if ENV['CI']
+  require 'simplecov-cobertura'
+  SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
 end
+
+SimpleCov.start
 
 require_relative '../lib/r18n-desktop'
